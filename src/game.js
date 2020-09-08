3759dad3e04ee8d3ef8f4b7e2ba670996f4590ea
Fabio Leal <fabio.lael@gmail.com>
2020-09-08 02:04:19 -0300

    Update: Screen css. Add: javascript file, implement the game state and render function renderScreen

---
+++ src/game.js
@@ -0,0 +1,36 @@
+const screen = document.querySelector('#screen');
+const context = screen.getContext('2d');
+
+const game = {
+  players: {
+    player1: { x: 1, y: 1 },
+    player2: { x: 9, y: 9 },
+  },
+  fruits: {
+    fruit1: { x: 3, y: 1 },
+  },
+};
+
+renderScreen();
+
+function renderScreen() {
+  context.fillStyle = 'white';
+  context.fillRect(0, 0, 10, 10);
+
+  for (playerId in game.players) {
+    const player = game.players[playerId];
+    context.fillStyle = 'black';
+    context.fillRect(player.x, player.y, 1, 1);
+  }
+
+  for (fruitId in game.fruits) {
+    const fruit = game.fruits[fruitId];
+    context.fillStyle = 'green';
+    context.fillRect(fruit.x, fruit.y, 1, 1);
+  }
+
+  requestAnimationFrame(renderScreen);
+}
+
+context.fillStyle = color;
+context.fillRect(positionX, positionY, width, height);
